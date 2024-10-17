830f88c1d35a2087fb5f210acff4c92f233ed706
2dam <criceyt@gmail.com>
2024-10-17 08:29:24 +0200

    checkbox hecho

--- RETO/src/userInterfaceTier/Application.java
+++ RETO/src/userInterfaceTier/Application.java
@@ -26,7 +26,7 @@ public class Application extends javafx.application.Application {
         
         // Asignamos una altura y anchura
         stage.setWidth(900);
-        stage.setHeight(700);
+        stage.setHeight(780);
         
         // Ponemos titulo a la app
         stage.setTitle("Sign up & Sign in");
